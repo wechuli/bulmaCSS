2936130e874857e7f46d19196e0e89d56d1fde9b
Wechuli <wechulipaul@yahoo.com>
2019-02-22 00:03:59 +0300

    implementing responsive header in react

--- bulma-course/src/components/navbar/src/App.jsx
+++ bulma-course/src/components/navbar/src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
+import NavBar1 from "./components/NavBar1";
+import Header from "./components/Header";
 
-
-const App = ()=>{
-  return(
+const App = () => {
+  return (
     <React.Fragment>
-      <p>
-        Hi
-      </p>
+      <NavBar1 />
+    
     </React.Fragment>
-  )
-}
-
+  );
+};
 
 export default App;
