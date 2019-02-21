27c04cb2d8f5e25976713208c13097e3569bacff
Wechuli <wechulipaul@yahoo.com>
2019-02-22 00:24:23 +0300

    image

--- bulma-course/src/components/navbar/src/App.jsx
+++ bulma-course/src/components/navbar/src/App.jsx
@@ -6,6 +6,17 @@ const App = () => {
   return (
     <React.Fragment>
       <NavBar1 />
+      <div style={{height:'1000px'}}  className="full-body">
+      <div className="columns">
+      <div className="column is-8">
+      </div>
+      <div className="column is-4">
+      <p className="content m-t-md">
+      And you as well must die beloved dust, and all your beauty stand you in no stead, that flawless vit
+      </p>
+      </div>
+      </div>
+      </div>
     
     </React.Fragment>
   );
